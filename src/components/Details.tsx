5de25220560845d0d336c3dad20d336033777516
Somchai <somchai.b@cvc.ac.th>
2024-08-15 19:44:42 +0700

    refactor: Optimize image loading in Details component

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -12,7 +12,7 @@ import type { PokemonResponseSingle } from "@/interface/pokemon.interface";
 import TypePokemon from "@/components/TypePokemon";
 import MiniInfo from "@/components/MiniInfo";
 import NotFound from "@/components/NotFound";
-import Loading from '@/components/Loading';
+import Loading from "@/components/Loading";
 
 type Props = {};
 
@@ -35,73 +35,80 @@ export default function Details({}: Props) {
 
   return (
     <>
-      <div className="w-full grid grid-cols-1 md:grid-cols-2 rounded-xl overflow-hidden bg-white">
-        <div className="">
-          <div className="flex justify-center p-6 ">
-            <Image
-              src={data.pokemon.image}
-              alt={data.pokemon.name}
-              className="rounded-lg"
-              width={400}
-              height={400}
-            />
-          </div>
+      <div className=" rounded-xl overflow-hidden bg-white p-4">
+        <div className="p-4">
+          <Link href="/" className="text-blue-500 hover:underline">
+            Back to list
+          </Link>
         </div>
-        <div className="p-4 flex flex-col gap-4">
-          <h1 className="text-6xl">{data.pokemon.name}</h1>
-          <div>
-            <div className=" flex flex-wrap flex-row gap-4 ">
-              {data.pokemon.types.map((type) => (
-                <TypePokemon type={type} key={type} />
-              ))}
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 mt-4">
+          <div className="">
+            <div className="flex justify-center ">
+              <Image
+                src={data.pokemon.image}
+                alt={data.pokemon.name}
+                className="rounded-lg"
+                width={400}
+                height={400}
+              />
             </div>
           </div>
-          <div className="flex flex-col gap-3">
-            <h2 className="text-3xl">Attacks</h2>
-            <div className="pl-4">
-              <h3 className="text-xl">Fast</h3>
-              <ul className="pl-3">
-                {data.pokemon.attacks.fast.map((attack) => (
-                  <li key={attack.name}>
-                    {attack.name} - {attack.damage}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="pl-4">
-              <h3 className="text-xl">Special</h3>
-              <ul className="pl-3">
-                {data.pokemon.attacks.special.map((attack) => (
-                  <li key={attack.name}>
-                    {attack.name} - {attack.damage}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          {data.pokemon.evolutions && data.pokemon.evolutions.length > 0 ? (
+          <div className="flex flex-col gap-4">
+            <h1 className="text-6xl">{data.pokemon.name}</h1>
             <div>
-              <h2 className="text-3xl">Evolutions</h2>
-              <div className="flex flex-row gap-4 mt-4">
-                {data.pokemon.evolutions.map((evolution) => (
-                  <Link
-                    key={evolution.id}
-                    href={`/?q=${evolution.name}`}
-                    className="hover:scale-110"
-                  >
-                    <MiniInfo pokemon={evolution} />
-                  </Link>
+              <div className=" flex flex-wrap flex-row gap-4 ">
+                {data.pokemon.types.map((type) => (
+                  <TypePokemon type={type} key={type} />
                 ))}
               </div>
             </div>
-          ) : (
-            <div>
-              <h2 className="text-3xl">Evolutions</h2>
-              <div className="flex flex-row gap-4 mt-4">
-                <p>This Pokémon does not have evolutions</p>
+            <div className="flex flex-col gap-3">
+              <h2 className="text-3xl">Attacks</h2>
+              <div className="pl-4">
+                <h3 className="text-xl">Fast</h3>
+                <ul className="pl-3">
+                  {data.pokemon.attacks.fast.map((attack) => (
+                    <li key={attack.name}>
+                      {attack.name} - {attack.damage}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="pl-4">
+                <h3 className="text-xl">Special</h3>
+                <ul className="pl-3">
+                  {data.pokemon.attacks.special.map((attack) => (
+                    <li key={attack.name}>
+                      {attack.name} - {attack.damage}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
-          )}
+            {data.pokemon.evolutions && data.pokemon.evolutions.length > 0 ? (
+              <div>
+                <h2 className="text-3xl">Evolutions</h2>
+                <div className="flex flex-row gap-4 mt-4">
+                  {data.pokemon.evolutions.map((evolution) => (
+                    <Link
+                      key={evolution.id}
+                      href={`/?q=${evolution.name}`}
+                      className="hover:scale-110"
+                    >
+                      <MiniInfo pokemon={evolution} />
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div>
+                <h2 className="text-3xl">Evolutions</h2>
+                <div className="flex flex-row gap-4 mt-4">
+                  <p>This Pokémon does not have evolutions</p>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
