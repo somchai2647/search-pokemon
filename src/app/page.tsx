316a30b108ddf8adfebf2e946c4967813bd260b7
Somchai <somchai.b@cvc.ac.th>
2024-08-15 15:17:06 +0700

    chore: Update npm dependencies and add Pokemon search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
-import Searchbar from "@/components/Searchbar";
+"use client";
 
 export default function Home() {
-  return (
-    <main className="p-32">
-      <h1 className="text-4xl font-bold text-center my-4">Pokemon Search</h1>
-      <Searchbar />
-    </main>
-  );
+  return <>Home</>;
 }
