31afeac0d2657bf6fcdb2d7dc62448b1ab5211ec
Somchai <somchai.b@cvc.ac.th>
2024-08-15 17:12:47 +0700

    refactor: Fix MiniInfo component styling for better alignment

--- src/components/MiniInfo.tsx
+++ src/components/MiniInfo.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 export default function MiniInfo({ pokemon }: Props) {
   return (
-    <div className="flex flex-col gap-1 justify-center drop-shadow-lg p-2 bg-white rounded-xl hover:cursor-pointer">
+    <div className="flex flex-col gap-1  justify-center drop-shadow-lg p-2 bg-white rounded-xl hover:cursor-pointer">
       <Image src={pokemon.image} alt={pokemon.name} width={100} height={100} />
       <p className="text-center font-bold">{pokemon.name}</p>
       <div className="flex gap-1">
